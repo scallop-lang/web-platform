819740d734ddf9b873eab8702e2936220af0bd84
Charles Wang <czw@seas.upenn.edu>
2023-08-06 00:26:49 -0400

    add run button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { PlayCircle } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 const Playground = () => {
   return (
@@ -8,15 +10,26 @@ const Playground = () => {
       <header className="flex flex-row items-center gap-3 bg-zinc-100 p-3">
         <Link href="/">
           <Image
+            className="transition hover:brightness-110"
             src="/content/logo.svg"
             width={50}
             height={50}
             alt="Scallop logo"
           />
         </Link>
-        <h1 className="text-2xl font-semibold">Scallop Playground</h1>
+        <h1 className="select-none text-2xl font-semibold">
+          Scallop Playground
+        </h1>
       </header>
-      <div className="flex items-center justify-center">Hello world!</div>
+      <main className="flex items-center justify-center">
+        <Button
+          className="bg-pink-300 text-black hover:bg-pink-400"
+          onClick={() => alert("program should run!")}
+        >
+          <PlayCircle className="mr-2 h-5 w-5" />
+          <span className="text-base">Run Program</span>
+        </Button>
+      </main>
     </div>
   );
 };
