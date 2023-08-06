fd9bf8663e39608a462dd32a9ce3cecdec2feb95
Charles Wang <czw@seas.upenn.edu>
2023-08-06 02:08:41 -0400

    editor as separate component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { PlayCircle } from "lucide-react";
+import { FileDown, PlayCircle } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 
 const Header = () => {
@@ -21,29 +22,53 @@ const Header = () => {
   );
 };
 
+const CodeEditor = () => {
+  const [code, setCode] = useState(
+    `// this will eventually be a fully functioning code editor.\n// for now, it holds some placeholder text I put in.\n// currently it's just a <textarea>. feel free to edit.\n\nrel name(a, b) :- name(a, c), is_a(c, b)\nrel num_animals(n) :- n = count(o: name(o, "animal"))\n\n// of course, the real editor will have syntax highlighting.`
+  );
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <Button
+          className="bg-pink-300 text-black hover:bg-pink-400"
+          onClick={() =>
+            alert("should eventually run the following code:\n\n" + code)
+          }
+        >
+          <PlayCircle className="mr-2 h-5 w-5" />
+          <span className="text-base">Run Program</span>
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            alert(
+              "should eventually download the following code, concatenated with stuff from the tables:\n\n" +
+                code
+            )
+          }
+        >
+          <FileDown className="mr-2 h-5 w-5" />
+          <span className="text-base">Download raw Scallop (.scl) file</span>
+        </Button>
+      </div>
+      <div className="grow rounded-md bg-zinc-200 p-4">
+        <textarea
+          className="h-full w-full resize-none bg-inherit font-mono text-base"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
 const Playground = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <main className="grid h-[calc(100vh-74px)] grid-cols-1 gap-8 p-8 lg:grid-cols-2">
-        <div className="flex flex-col bg-red-100">
-          <div className="flex items-center justify-between bg-blue-100">
-            <Button
-              className="bg-pink-300 text-black hover:bg-pink-400"
-              onClick={() => alert("program should run!")}
-            >
-              <PlayCircle className="mr-2 h-5 w-5" />
-              <span className="text-base">Run Program</span>
-            </Button>
-            <Button
-              variant="link"
-              className="text-base"
-              onClick={() => alert("download file!")}
-            >
-              Download raw Scallop (.scl) file
-            </Button>
-          </div>
-        </div>
+        <CodeEditor />
         <div className="bg-green-100">Test</div>
       </main>
     </div>
