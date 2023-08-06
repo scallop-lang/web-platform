18028d55f9d4e5d48808b9c7fd3647baeabe03f8
Charles Wang <czw@seas.upenn.edu>
2023-08-06 01:23:24 -0400

    separate header into component and begin skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,46 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
+const Header = () => {
+  return (
+    <header className="flex max-h-min flex-row items-center gap-3 bg-zinc-100 p-3">
+      <Link href="/">
+        <Image
+          className="transition hover:brightness-110"
+          src="/content/logo.svg"
+          width={50}
+          height={50}
+          alt="Scallop logo"
+        />
+      </Link>
+      <h1 className="select-none text-2xl font-semibold">Scallop Playground</h1>
+    </header>
+  );
+};
+
 const Playground = () => {
   return (
     <div className="min-h-screen">
-      <header className="flex flex-row items-center gap-3 bg-zinc-100 p-3">
-        <Link href="/">
-          <Image
-            className="transition hover:brightness-110"
-            src="/content/logo.svg"
-            width={50}
-            height={50}
-            alt="Scallop logo"
-          />
-        </Link>
-        <h1 className="select-none text-2xl font-semibold">
-          Scallop Playground
-        </h1>
-      </header>
-      <main className="flex items-center justify-center">
-        <Button
-          className="bg-pink-300 text-black hover:bg-pink-400"
-          onClick={() => alert("program should run!")}
-        >
-          <PlayCircle className="mr-2 h-5 w-5" />
-          <span className="text-base">Run Program</span>
-        </Button>
+      <Header />
+      <main className="grid h-[calc(100vh-74px)] grid-cols-1 gap-8 p-8 lg:grid-cols-2">
+        <div className="flex flex-col bg-red-100">
+          <div className="flex items-center justify-between bg-blue-100">
+            <Button
+              className="bg-pink-300 text-black hover:bg-pink-400"
+              onClick={() => alert("program should run!")}
+            >
+              <PlayCircle className="mr-2 h-5 w-5" />
+              <span className="text-base">Run Program</span>
+            </Button>
+            <Button
+              variant="link"
+              className="text-base"
+            >
+              Download raw <span className="font-mono">.scl</span> file
+            </Button>
+          </div>
+        </div>
+        <div className="bg-green-100">Test</div>
       </main>
     </div>
   );
