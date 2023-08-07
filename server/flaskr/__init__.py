b18aea8e3e972daee77a43240587728becfc6f50
GeneralPoxter <generalpoxter@gmail.com>
2023-08-06 22:18:04 -0400

    refactor

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -1,52 +1,15 @@
-from flask import Flask, request, jsonify
-from .config import config
-
-import scallopy
+from flask import Flask
 import os
+
+from .api import api_routes
+from .config import config
 
 
 def create_app(config_name=None):
     app = Flask(__name__)
+    app.register_blueprint(api_routes)
+
     config_name = config_name or os.environ.get("FLASK_ENV") or "default"
     app.config.from_object(config[config_name])
-
-    @app.route("/api/run-scallop", methods=["POST"])
-    def run_scallop():
-        request_data = request.get_json()
-        inputs, program, outputs = (
-            request_data["inputs"],
-            request_data["program"],
-            request_data["outputs"],
-        )
-
-        # Write logic program to scl file
-        if not os.path.exists(app.config["TMP_PATH"]):
-            os.makedirs(app.config["TMP_PATH"])
-        scallop_file = os.path.join(app.config["TMP_PATH"], "program.scl")
-        with open(scallop_file, "w") as f:
-            f.write(program)
-
-        # Initialize Scallop context
-        ctx = scallopy.ScallopContext(
-            provenance="topkproofs"
-        )  # should allow user to change provenance
-        ctx.import_file(scallop_file)
-
-        # Add input relations
-        for relation in inputs:
-            ctx.add_relation(relation["name"], (str, str))
-            ctx.add_facts(
-                relation["name"], [(tag, tuple(tup)) for tag, tup in relation["facts"]]
-            )
-
-        # Execute the program
-        ctx.run()
-
-        # Extract output relations
-        data = []
-        for relation in outputs:
-            data.append(list(ctx.relation(relation)))
-
-        return jsonify(data)
 
     return app
---
+++ server/flaskr/api.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, request, current_app, jsonify
+import scallopy
+import os
+
+api_routes = Blueprint("api_routes", __name__)
+
+
+@api_routes.route("/api/run-scallop", methods=["POST"])
+def run_scallop():
+    request_data = request.get_json()
+    inputs, program, outputs = (
+        request_data["inputs"],
+        request_data["program"],
+        request_data["outputs"],
+    )
+
+    # Write logic program to scl file
+    tmp_path = current_app.config["TMP_PATH"]
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
+    scallop_file = os.path.join(tmp_path, "program.scl")
+    with open(scallop_file, "w") as f:
+        f.write(program)
+
+    # Initialize Scallop context
+    ctx = scallopy.ScallopContext(
+        provenance="topkproofs"
+    )  # should allow user to change provenance
+    ctx.import_file(scallop_file)
+
+    # Add input relations
+    for relation in inputs:
+        ctx.add_relation(relation["name"], (str, str))
+        ctx.add_facts(
+            relation["name"], [(tag, tuple(tup)) for tag, tup in relation["facts"]]
+        )
+
+    # Execute the program
+    ctx.run()
+
+    # Extract output relations
+    data = []
+    for relation in outputs:
+        data.append(list(ctx.relation(relation)))
+
+    return jsonify(data)
