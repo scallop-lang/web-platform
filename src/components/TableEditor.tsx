c1c8a73fb644b9a95e502469ca88ae48e793278f
Anthony <ge01020g@gmail.com>
2023-08-06 18:29:11 -0700

    added basic functionality
    it can handle a whopping two data types

--- src/components/TableEditor.tsx
+++ src/components/TableEditor.tsx
@@ -22,9 +22,17 @@ import { Switch } from "~/components/ui/switch";
 import { Button } from "~/components/ui/button";
 import { PlusSquare } from 'lucide-react';
 
+interface ColumnField {
+    id: number;
+    name: string;
+    type: string;
+}
+
 interface Relation {
     id: number;
     name: string;
+    columns: ColumnField[];
+    rows: Record<string, string>[];
     // column array, each element is a column component
     // row array, each element is a row component
 }
@@ -37,6 +45,7 @@ const TableSelect = ({ jsonArray } : {jsonArray : Relation[]}) => {
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
+            <SelectLabel>Select a relationship to see</SelectLabel>
             {jsonArray.map((rel, index) => (
                 <SelectItem key={index} value={rel.name + rel.id}>
                   <span className="font-mono">{rel.name}</span>
@@ -50,6 +59,8 @@ const TableSelect = ({ jsonArray } : {jsonArray : Relation[]}) => {
 
 function TableEditor() {
     const [jsonArray, setJsonArray] = useState<Relation[]>([]);; // this stores the relations
+    const [colArray, setColArray] = useState<ColumnField[]>([]);;
+    const [rowArray, setRowArray] = useState<Record<string, string>[]>([]); // kind of useless atm
     const [open, setOpen] = useState(false); // toggle dialog box
     const [newItemName, setNewItemName] = useState('');
 
@@ -58,13 +69,42 @@ function TableEditor() {
     }, [jsonArray]);
 
     function addItem(){
-        const newItem: Relation = {
-            id: Date.now(),
-            name: newItemName,
-        };
-        setJsonArray(prevState => [...prevState, newItem]);
-        setOpen(false);
-        setNewItemName('');
+      const newItem: Relation = {
+          id: Date.now(),
+          name: newItemName,
+          columns: colArray,
+          rows: rowArray
+      };
+      setJsonArray(prevState => [...prevState, newItem]);
+      resetItem();
+    }
+
+    function resetItem(){
+      setOpen(false);
+      setNewItemName('');
+      setColArray([]);
+    }
+
+    function addColumnField(){
+      const newColumnField : ColumnField = {
+          id: Date.now(),
+          name: '',
+          type: "String",
+      };
+      setColArray(prevState => [...prevState, newColumnField]);
+    }
+
+    function removeColumnField({i} : {i : number}){
+        const newColArray = [...colArray];
+        newColArray.splice(i, 1);
+        setColArray(newColArray);
+    }
+
+    function titleChange({column, title} : {column: ColumnField, title: string}){
+      column.name = title;
+    }
+    function typeChange({column, type} : {column: ColumnField, type: string}){
+      column.type = type;
     }
 
     return(
@@ -80,20 +120,54 @@ function TableEditor() {
                   <DialogContent>
                       <DialogHeader>
                           <DialogTitle>Create Relation</DialogTitle>
-                          <DialogDescription>
-                          This is where you&apos;ll be creating and editing your relation
-                          signatures/arguments. Work in progress.
-                          </DialogDescription>
+                          <input
+                            type="text"
+                            value={newItemName}
+                            onChange={(e) => setNewItemName(e.target.value)}
+                            placeholder="Enter the name here..."
+                            className="bg-neutral-200"
+                          />
+                          <DialogHeader>
+                            <DialogTitle>Arguments</DialogTitle>
+                            <DialogDescription>Enter an optional name, datatype, then rearrange the order.</DialogDescription>
+                          </DialogHeader>
                       </DialogHeader>
-                      <div>
-                          <DialogFooter>
-                              <input
-                                  type="text"
-                                  value={newItemName}
-                                  onChange={(e) => setNewItemName(e.target.value)}
-                                  placeholder="Enter item name"
-                              />
-                              <button onClick={addItem}>Confirm</button>
+                      <div className="overflow-hidden">
+                          <div className="columnAdjuster">
+
+                            <div className="mb-2 flex items-center">
+                              <Button onClick={addColumnField} className="shrink-0 bg-sky-300 text-black hover:bg-sky-400 h-10 w-10">
+                              </Button>
+                              <p className="ml-2">Add your arguments here</p>
+                            </div>
+                            <div className="columnList">
+                              {colArray.map((columnField, index) => (
+                                <div className="flex justify-between mb-2" key={index}>
+                                  <input
+                                    type="text"
+                                    onChange={(e) => (titleChange({column : columnField, title : e.target.value}))}
+                                    placeholder="Enter the title here..."
+                                    className="bg-neutral-200"
+                                  />
+                                  <Select defaultValue={"String"} onValueChange={(e) => (typeChange({column : columnField, type: e}))}>
+                                    <SelectTrigger className="w-[180px]">
+                                      <SelectValue placeholder="Select a type" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                      <SelectGroup>
+                                        <SelectItem value="String">String</SelectItem>
+                                        <SelectItem value="Int32">Int-32</SelectItem>
+                                      </SelectGroup>
+                                    </SelectContent>
+                                  </Select>
+                                  <Button onClick={() => removeColumnField({i : index})}>Delete</Button>
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                          <DialogFooter className="mt-6">
+                            <Button onClick={resetItem}>Delete</Button>
+                            <Button onClick={addItem} className="shrink-0 bg-sky-300 text-black hover:bg-sky-400">Confirm</Button>
                           </DialogFooter>
                       </div>
                   </DialogContent>
