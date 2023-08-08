1014182bf2fd1b70ce6b39ddcb3b2c91d5d27051
Charles Wang <czw@seas.upenn.edu>
2023-08-08 03:47:05 -0400

    rework download process

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,9 +1,70 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { FileDown, PlayCircle } from "lucide-react";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { type ScallopProgram } from "~/server/api/routers/scallop";
 import { download } from "../utils/download";
 import { Card } from "./ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+const DownloadButton = ({ program }: { program: string }) => {
+  const [filename, setFilename] = useState("raw");
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <FileDown className="mr-2 h-5 w-5" />
+          <span className="text-base">Download raw file</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Download</DialogTitle>
+          <DialogDescription className="text-base">
+            This will download a Scallop (.scl) file containing any program code
+            you&apos;ve written, along with your input and output tables. Table
+            content is appended to the file.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-1.5">
+          <Label
+            className="text-base"
+            htmlFor="filename"
+          >
+            Name your file
+          </Label>
+          <Input
+            type="text"
+            placeholder="Filename"
+            id="filename"
+            value={filename}
+            onChange={(e) => setFilename(e.target.value)}
+          />
+        </div>
+        <DialogFooter>
+          <Button
+            className="bg-pink-300 text-black hover:bg-pink-400"
+            onClick={() => download(program, filename)}
+          >
+            <FileDown className="mr-2 h-5 w-5" />
+            <span className="text-base">Download</span>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 const CodeToolbar = ({ program }: { program: string }) => {
   return (
@@ -15,13 +76,7 @@ const CodeToolbar = ({ program }: { program: string }) => {
         <PlayCircle className="mr-2 h-5 w-5" />
         <span className="text-base">Run program</span>
       </Button>
-      <Button
-        variant="outline"
-        onClick={() => download(program)}
-      >
-        <FileDown className="mr-2 h-5 w-5" />
-        <span className="text-base">Download raw Scallop (.scl) file</span>
-      </Button>
+      <DownloadButton program={program} />
     </div>
   );
 };
--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,9 +1,9 @@
-export function download(content: string) {
+export function download(content: string, filename: string) {
   const a = document.createElement("a");
   const url = URL.createObjectURL(new Blob([content], { type: "text/plain" }));
 
   a.href = url;
-  a.download = "raw.scl";
+  a.download = filename + ".scl";
   a.click();
 
   URL.revokeObjectURL(url);
