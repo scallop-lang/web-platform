573d3262f43d0582f79797e8a1b3615dadb5ee47
GeneralPoxter <generalpoxter@gmail.com>
2023-08-09 15:24:06 -0400

    simplify type schema

--- src/utils/schemas-types.ts
+++ src/utils/schemas-types.ts
@@ -2,21 +2,13 @@ import { z } from "zod";
 
 const NameSchema = z.string().regex(new RegExp("^[a-z]\\w*$", "i"))
 
-// the current Scallop types we support
-const ArgTypeSchema = z.union([
-  z.literal("String"),
-  z.literal("Integer"),
-  z.literal("Float"),
-  z.literal("Boolean"),
-]);
-
-// exported so that we can iterate over them. also see
+// the current Scallop types we support; exported so that we can iterate over them. also see
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
 const argumentTypes = ["String", "Integer", "Float", "Boolean"] as const;
 
 const ArgSchema = z.object({
   name: NameSchema.optional(),
-  type: ArgTypeSchema,
+  type: z.enum(argumentTypes),
 });
 
 const SclRelationSchema = z.object({
@@ -29,7 +21,6 @@ const SclProgramSchema = z.string().min(1);
 const SclInputSchema = SclRelationSchema.required();
 const SclOutputSchema = SclRelationSchema.omit({ facts: true });
 
-type ArgumentType = z.infer<typeof ArgTypeSchema>;
 type Argument = z.infer<typeof ArgSchema>;
 type ScallopProgram = z.infer<typeof SclProgramSchema>;
 type ScallopInput = z.infer<typeof SclInputSchema>;
@@ -39,7 +30,6 @@ type OutputRecord = Record<string, ScallopOutput>;
 
 export type {
   Argument,
-  ArgumentType,
   InputRecord,
   OutputRecord,
   ScallopInput,
@@ -49,7 +39,6 @@ export type {
 
 export {
   ArgSchema,
-  ArgTypeSchema,
   SclInputSchema,
   SclOutputSchema,
   SclProgramSchema,
