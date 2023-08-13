2fa14736ffb77744f51b6c7c1f0f49f2b41fb163
Charles Wang <czw@seas.upenn.edu>
2023-08-13 17:08:35 -0400

    remove TableCell
    when we have to pass that many props to the component,
    it kinda defeats the purpose

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -2,12 +2,7 @@ import { ListPlus, ListX, MoreVertical, Settings2 } from "lucide-react";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { cn } from "~/utils/cn";
-import type {
-  Argument,
-  Fact,
-  RelationRecord,
-  SclRelation,
-} from "~/utils/schemas-types";
+import type { Fact, RelationRecord, SclRelation } from "~/utils/schemas-types";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import {
@@ -148,75 +143,15 @@ const TableHeader = ({ relation }: { relation: SclRelation }) => {
   );
 };
 
-const TableCell = ({
-  relation,
-  record,
-  argIndex,
-  fact,
-  argument,
-  setRecord,
-}: {
-  relation: SclRelation;
-  record: RelationRecord;
-  argIndex: number;
-  fact: Fact;
-  argument: Argument;
-  setRecord: React.Dispatch<React.SetStateAction<RelationRecord>>;
-}) => {
-  function updateCell(value: string) {
-    setRecord({
-      ...record,
-      [relation.name]: {
-        ...relation,
-        facts: relation.facts.map((f) => {
-          if (f.id === fact.id) {
-            f.tuple[argIndex] = value;
-          }
-          return f;
-        }),
-      },
-    });
-  }
-
-  const initialState = fact.tuple[argIndex]!;
-  const isInput = relation.type === "input";
-
-  // for now, the other types will use the same input field
-  switch (argument.type) {
-    case "Boolean":
-      return (
-        <BooleanCell
-          initialState={initialState === "true"}
-          updateCell={updateCell}
-          relationType={relation.type}
-        />
-      );
-    default:
-      return (
-        <Input
-          type="text"
-          defaultValue={initialState}
-          onChange={(e) => updateCell(e.target.value)}
-          placeholder={argument.type}
-          className={cn(
-            "transition hover:bg-secondary focus:bg-background",
-            isInput ? "cursor-pointer focus:cursor-text" : "cursor-default"
-          )}
-          readOnly={!isInput}
-        />
-      );
-  }
-};
-
 const TableRow = ({
   relation,
   record,
-  fact,
+  rowFact,
   setRecord,
 }: {
   relation: SclRelation;
   record: RelationRecord;
-  fact: Fact;
+  rowFact: Fact;
   setRecord: React.Dispatch<React.SetStateAction<RelationRecord>>;
 }) => {
   function deleteRow() {
@@ -224,29 +159,66 @@ const TableRow = ({
       ...record,
       [relation.name]: {
         ...relation,
-        facts: relation.facts.filter((f) => f.id !== fact.id),
+        facts: relation.facts.filter((f) => f.id !== rowFact.id),
       },
     });
   }
 
-  const colList = relation.args.map((argument, argIndex) => (
+  const colList = relation.args.map((argument, argIndex) => {
+    function updateCell(value: string) {
+      setRecord({
+        ...record,
+        [relation.name]: {
+          ...relation,
+          facts: relation.facts.map((fact) => {
+            if (fact.id === rowFact.id) {
+              fact.tuple[argIndex] = value;
+            }
+            return fact;
+          }),
+        },
+      });
+    }
+
+    const initialState = rowFact.tuple[argIndex]!;
+    const isInput = relation.type === "input";
+
+    // for now, the other types will use the same input field.
+    //
     // we only need to differentiate between table cell siblings (the row). this
-    // means using the same argument.id across a column is fine!
-    <TableCell
-      key={argument.id}
-      relation={relation}
-      record={record}
-      argIndex={argIndex}
-      fact={fact}
-      argument={argument}
-      setRecord={setRecord}
-    />
-  ));
+    // means using the same argument.id across a column is fine (not siblings)!
+    switch (argument.type) {
+      case "Boolean":
+        return (
+          <BooleanCell
+            key={argument.id}
+            initialState={initialState === "true"}
+            updateCell={updateCell}
+            relationType={relation.type}
+          />
+        );
+      default:
+        return (
+          <Input
+            key={argument.id}
+            type="text"
+            defaultValue={initialState}
+            onChange={(e) => updateCell(e.target.value)}
+            placeholder={argument.type}
+            className={cn(
+              "transition hover:bg-secondary focus:bg-background",
+              isInput ? "cursor-pointer focus:cursor-text" : "cursor-default"
+            )}
+            readOnly={!isInput}
+          />
+        );
+    }
+  });
 
   return (
     <div
       className="flex space-x-2"
-      key={fact.id}
+      key={rowFact.id}
     >
       {colList}
       {relation.type === "input" ? (
@@ -294,7 +266,7 @@ const Table = ({
       key={row}
       relation={relation}
       record={record}
-      fact={fact}
+      rowFact={fact}
       setRecord={setRecord}
     />
   ));
