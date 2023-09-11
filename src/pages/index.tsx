0696bfd11ffa219ff700afdcb8fae05ed7f7c523
Charles Wang <czw@seas.upenn.edu>
2023-09-11 19:25:03 -0400

    rearrange routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,13 @@
-import { Plus } from "lucide-react";
-import Head from "next/head";
-import Link from "next/link";
-import Header from "~/components/header";
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import Playground from "~/components/playground";
+import ScallopHead from "~/components/scallop-head";
 
-const App = () => {
+const Root = () => {
   return (
     <>
-      <Head>
-        <title>Scallop Playground</title>
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href="/favicon.svg"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon.png"
-        />
-      </Head>
-      <Header />
-      <main className="flex min-h-screen flex-col space-y-10 bg-background p-5 lg:p-8">
-        <div className="flex flex-col space-y-1.5">
-          <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
-            Your projects
-          </h3>
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>New project</CardTitle>
-              <CardDescription>
-                Start an empty project from scratch. Just the defaults. No
-                relations defined.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Button asChild>
-                <Link href="/new">
-                  <Plus className="mr-2 h-4 w-4" />
-                  Create
-                </Link>
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="flex flex-col space-y-1.5">
-          <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
-            AAAI demos
-          </h3>
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Coming soon...</CardTitle>
-              <CardDescription>lol</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Button asChild>
-                <Link href="/new">
-                  <Plus className="mr-2 h-4 w-4" />
-                  Create
-                </Link>
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
-      </main>
+      <ScallopHead />
+      <Playground />
     </>
   );
 };
 
-export default App;
+export default Root;
