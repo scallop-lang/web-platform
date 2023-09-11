aa705debba2e095a824c44406687f55a4424a95e
Charles Wang <czw@seas.upenn.edu>
2023-09-11 19:15:09 -0400

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,24 @@
-import { Laptop2, Moon, Sun } from "lucide-react";
+import { Laptop2, LogIn, Moon, Sun, User } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { Button } from "./ui/button";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Skeleton } from "./ui/skeleton";
 
-const AppearanceSwitcher = () => {
+const AvatarDropdown = () => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
 
@@ -24,44 +28,55 @@ const AppearanceSwitcher = () => {
   useEffect(() => setMounted(true), []);
 
   const resolvedIcon = !mounted ? (
-    <Skeleton className="h-4 w-4 rounded-full" />
+    <Skeleton className="mr-2 h-4 w-4 rounded-full" />
   ) : resolvedTheme === "light" ? (
-    <Sun className="h-4 w-4" />
+    <Sun className="mr-2 h-4 w-4" />
   ) : (
-    <Moon className="h-4 w-4" />
+    <Moon className="mr-2 h-4 w-4" />
   );
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          size="icon"
-          variant="outline"
-          aria-haspopup
-        >
-          {resolvedIcon}
-        </Button>
+        <Avatar className="h-9 w-9">
+          <AvatarFallback className="cursor-pointer">
+            <User className="h-5 w-5" />
+          </AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          <p>Guest</p>
+          <p className="text-sm font-normal text-muted-foreground">
+            Log in to save your progress.
+          </p>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem
-          onClick={() => setTheme("light")}
-          className="justify-between"
-        >
-          Light <Sun className="h-4 w-4" />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => setTheme("dark")}
-          className="justify-between"
-        >
-          Dark <Moon className="h-4 w-4" />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => setTheme("system")}
-          className="justify-between"
-        >
-          System <Laptop2 className="h-4 w-4" />
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            {resolvedIcon} Appearance
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                <Sun className="mr-2 h-4 w-4" />
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <Moon className="mr-2 h-4 w-4" />
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                <Laptop2 className="mr-2 h-4 w-4" />
+                System
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <LogIn className="mr-2 h-4 w-4" />
+          Login
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
@@ -70,37 +85,44 @@ const AppearanceSwitcher = () => {
 
 const Header = () => {
   return (
-    <header className="flex w-full items-center justify-between border-b border-border bg-neutral-50 p-3 dark:bg-neutral-950">
-      <div className="flex items-center space-x-3">
-        <Image
-          src="/content/logo.svg"
-          width={25}
-          height={25}
-          alt="Scallop logo"
-        />
-        <h1 className="w-28 cursor-default text-lg font-semibold leading-none sm:w-full sm:text-xl">
-          Playground
-        </h1>
-      </div>
-      <div className="flex items-center space-x-7">
-        <div className="flex flex-col items-end justify-end sm:flex-row sm:space-x-7">
-          <Link
-            href="https://scallop-lang.github.io/"
-            target="_blank"
-            className="inline-flex items-center justify-center text-right text-sm font-medium text-primary underline-offset-4 hover:underline"
-          >
-            Website
-          </Link>
-          <Link
-            href="https://github.com/scallop-lang"
-            target="_blank"
-            className="inline-flex items-center justify-center text-sm font-medium text-primary underline-offset-4 hover:underline"
-          >
-            GitHub
-          </Link>
-        </div>
-        <AppearanceSwitcher />
-      </div>
+    <header className="flex w-full items-center justify-between border-b border-border bg-background p-2">
+      <nav className="flex space-x-8">
+        <Link
+          href="/"
+          className="ml-1 flex items-center space-x-2 hover:underline"
+        >
+          <Image
+            width={25}
+            height={25}
+            src="/content/logo.svg"
+            alt="Scallop logo"
+          />
+          <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">
+            Playground
+          </h4>
+        </Link>
+        <Link
+          href="/examples"
+          className="flex items-center text-sm font-medium hover:underline"
+        >
+          Examples
+        </Link>
+        <Link
+          href="https://scallop-lang.github.io/doc/index.html"
+          target="_blank"
+          className="flex items-center text-sm font-medium hover:underline"
+        >
+          Language Docs
+        </Link>
+        <Link
+          href="https://scallop-lang.github.io/"
+          target="_blank"
+          className="flex items-center text-sm font-medium hover:underline"
+        >
+          Main Site
+        </Link>
+      </nav>
+      <AvatarDropdown />
     </header>
   );
 };
