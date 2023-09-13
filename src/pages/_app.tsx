8ab5dc81f96fd53f91bb65c349b300b31ffc630c
Charles Wang <czw@seas.upenn.edu>
2023-09-13 17:49:09 -0400

    user auth?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,53 @@
-import { type AppType } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import ThemeProvider from "~/components/theme-provider";
 import { Toaster } from "~/components/ui/toaster";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  preload: false,
+});
 
 const plexMono = IBM_Plex_Mono({
   weight: ["400", "700", "100", "200", "300", "500", "600"],
   style: ["normal", "italic"],
   subsets: ["latin"],
   variable: "--font-plex-mono",
+  preload: false,
 });
 
-const App: AppType = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps<{session: Session}>) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <style
-        jsx
-        global
-      >{`
-        :root {
-          font-family: ${inter.style.fontFamily};
-        }
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <style
+          jsx
+          global
+        >{`
+          :root {
+            font-family: ${inter.style.fontFamily};
+          }
 
-        .font-mono {
-          font-family: ${plexMono.style.fontFamily};
-        }
-      `}</style>
-      <div className={`${inter.variable} ${plexMono.variable} font-sans`}>
-        <Component {...pageProps} />
-        <Toaster />
-      </div>
-    </ThemeProvider>
+          .font-mono {
+            font-family: ${plexMono.style.fontFamily};
+          }
+        `}</style>
+        <div className={`${inter.variable} ${plexMono.variable} font-sans`}>
+          <Component {...pageProps} />
+          <Toaster />
+        </div>
+      </ThemeProvider>
+    </SessionProvider>
   );
 };
 
