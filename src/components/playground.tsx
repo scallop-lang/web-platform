a1514e3a98ac9e7a851106d1f49d0be32524caca
Charles Wang <czw@seas.upenn.edu>
2023-09-16 16:30:09 -0400

    dismissable alert

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -7,6 +7,37 @@ import CodeEditor from "./code-editor";
 import TableEditor from "./table-editor";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
+const DismissableAlert = () => {
+  const [closed, setClosed] = useState(false);
+
+  if (closed) {
+    return null;
+  }
+
+  return (
+    <Alert className="shrink-0">
+      <AlertTitle>Welcome to the Scallop playground!</AlertTitle>
+      <AlertDescription>
+        If you close this tab you&apos;ll lose your work. Make an account to
+        access the{" "}
+        <Link
+          href="/dashboard"
+          className="underline font-medium"
+        >
+          dashboard
+        </Link>{" "}
+        and save your projects.{" "}
+        <button
+          className="font-medium underline"
+          onClick={() => setClosed(true)}
+        >
+          Close this alert.
+        </button>
+      </AlertDescription>
+    </Alert>
+  );
+};
+
 const Playground = () => {
   const [program, setProgram] = useState<SclProgram>("");
   const [inputs, setInputs] = useState<RelationRecord>({});
@@ -14,14 +45,7 @@ const Playground = () => {
 
   return (
     <main className="flex flex-col h-[calc(100vh-53px)] gap-3 bg-background p-4">
-      <Alert className="shrink-0">
-        <AlertTitle>Welcome to the Scallop playground!</AlertTitle>
-        <AlertDescription>
-          If you close this tab you&apos;ll lose your work. Make an account to
-          access the <Link href="/dashboard">dashboard</Link> and save your
-          projects.
-        </AlertDescription>
-      </Alert>
+      <DismissableAlert />
       <div className="grid grid-cols-2 h-full gap-3">
         <CodeEditor
           inputs={inputs}
