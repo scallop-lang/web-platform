0b0503f561b50c41667ba4dce00922a56451bb65
Charles Wang <czw@seas.upenn.edu>
2023-09-16 16:11:52 -0400

    add alert

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import { useState } from "react";
 
 import { type RelationRecord, type SclProgram } from "~/utils/schemas-types";
 
 import CodeEditor from "./code-editor";
 import TableEditor from "./table-editor";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
 const Playground = () => {
   const [program, setProgram] = useState<SclProgram>("");
@@ -11,20 +13,30 @@ const Playground = () => {
   const [outputs, setOutputs] = useState<RelationRecord>({});
 
   return (
-    <main className="grid h-[calc(100vh-53px)] grid-cols-2 gap-3 bg-background p-4">
-      <CodeEditor
-        inputs={inputs}
-        outputs={outputs}
-        program={program}
-        setProgram={setProgram}
-        setOutputs={setOutputs}
-      />
-      <TableEditor
-        inputs={inputs}
-        outputs={outputs}
-        setInputs={setInputs}
-        setOutputs={setOutputs}
-      />
+    <main className="flex flex-col h-[calc(100vh-53px)] gap-3 bg-background p-4">
+      <Alert className="shrink-0">
+        <AlertTitle>Welcome to the Scallop playground!</AlertTitle>
+        <AlertDescription>
+          If you close this tab you&apos;ll lose your work. Make an account to
+          access the <Link href="/dashboard">dashboard</Link> and save your
+          projects.
+        </AlertDescription>
+      </Alert>
+      <div className="grid grid-cols-2 h-full gap-3">
+        <CodeEditor
+          inputs={inputs}
+          outputs={outputs}
+          program={program}
+          setProgram={setProgram}
+          setOutputs={setOutputs}
+        />
+        <TableEditor
+          inputs={inputs}
+          outputs={outputs}
+          setInputs={setInputs}
+          setOutputs={setOutputs}
+        />
+      </div>
     </main>
   );
 };
