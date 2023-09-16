1dee27b24ea77e8b457ba404a0e5a178adadb7eb
Anthony <geant@seas.upenn.edu>
2023-09-16 15:36:09 -0400

    stuff

--- src/utils/isvalidtype.ts
+++ src/utils/isvalidtype.ts
@@ -2,23 +2,25 @@ import type { Argument } from "~/utils/schemas-types";
 
 export function isValidType(value: string, argument: Argument) {
   const argumentType = argument.type;
+  //should be used for relation names
   if (argumentType === "String") {
     return !/^[0-9]/.test(value) && /^[A-Za-z0-9]*$/.test(value);
-    //should be used for relation names
   }
-  if (argumentType === "Integer" || argumentType === "Float") {
-    // check integer/float case
+  // check integer/float case
+  if (argumentType === "Integer" || argumentType === "Float") { 
+    // invalid number
     if (isNaN(+value)) {
       return false;
-    } // invalid number
+    }
+    // invalid integer
     if (argumentType === "Integer" && !Number.isInteger(+value)) {
       return false;
-    } // invalid Integer
+    }
     return true;
   }
   if (argumentType === "Boolean") {
     return value === "true" || value === "false";
   }
-  return false;
   // case where argumentType is none of these
+  return false;
 }
