71f5ce44f26826d9e62a4ac1d6dbe7a3b65c38fc
Charles Wang <czw@seas.upenn.edu>
2023-09-16 16:50:39 -0400

    further simplify playground

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -1,67 +1,39 @@
-import Link from "next/link";
 import { useState } from "react";
 
 import { type RelationRecord, type SclProgram } from "~/utils/schemas-types";
 
 import CodeEditor from "./code-editor";
 import TableEditor from "./table-editor";
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
-const DismissableAlert = () => {
-  const [closed, setClosed] = useState(false);
-
-  if (closed) {
-    return null;
-  }
-
-  return (
-    <Alert className="shrink-0">
-      <AlertTitle>Welcome to the Scallop playground!</AlertTitle>
-      <AlertDescription>
-        If you close this tab you&apos;ll lose your work. Make an account to
-        access the{" "}
-        <Link
-          href="/dashboard"
-          className="underline font-medium"
-        >
-          dashboard
-        </Link>{" "}
-        and save your projects.{" "}
-        <button
-          className="font-medium underline"
-          onClick={() => setClosed(true)}
-        >
-          Close this alert.
-        </button>
-      </AlertDescription>
-    </Alert>
-  );
-};
-
-const Playground = () => {
-  const [program, setProgram] = useState<SclProgram>("");
-  const [inputs, setInputs] = useState<RelationRecord>({});
-  const [outputs, setOutputs] = useState<RelationRecord>({});
+const Playground = ({
+  initProgram,
+  initInputs,
+  initOutputs,
+}: {
+  initProgram: SclProgram;
+  initInputs: RelationRecord;
+  initOutputs: RelationRecord;
+}) => {
+  const [program, setProgram] = useState<SclProgram>(initProgram);
+  const [inputs, setInputs] = useState<RelationRecord>(initInputs);
+  const [outputs, setOutputs] = useState<RelationRecord>(initOutputs);
 
   return (
-    <main className="flex flex-col h-[calc(100vh-53px)] gap-3 bg-background p-4">
-      <DismissableAlert />
-      <div className="grid grid-cols-2 h-full gap-3">
-        <CodeEditor
-          inputs={inputs}
-          outputs={outputs}
-          program={program}
-          setProgram={setProgram}
-          setOutputs={setOutputs}
-        />
-        <TableEditor
-          inputs={inputs}
-          outputs={outputs}
-          setInputs={setInputs}
-          setOutputs={setOutputs}
-        />
-      </div>
-    </main>
+    <div className="grid grid-cols-2 h-full gap-3">
+      <CodeEditor
+        inputs={inputs}
+        outputs={outputs}
+        program={program}
+        setProgram={setProgram}
+        setOutputs={setOutputs}
+      />
+      <TableEditor
+        inputs={inputs}
+        outputs={outputs}
+        setInputs={setInputs}
+        setOutputs={setOutputs}
+      />
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,53 @@
+import { PartyPopper } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
 import Playground from "~/components/playground";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
+
+const WelcomeAlert = () => {
+  const [closed, setClosed] = useState(false);
+
+  if (closed) {
+    return null;
+  }
+
+  return (
+    <Alert className="shrink-0">
+      <PartyPopper className="mr-2 w-5 h-5" />
+      <AlertTitle>Welcome to Scallop!</AlertTitle>
+      <AlertDescription>
+        If you close the playground all work will be lost. Make an account to
+        access the{" "}
+        <Link
+          href="/dashboard"
+          className="underline font-medium"
+        >
+          dashboard
+        </Link>{" "}
+        and to save your projects.{" "}
+        <button
+          className="font-medium underline"
+          onClick={() => setClosed(true)}
+        >
+          Close this alert.
+        </button>
+      </AlertDescription>
+    </Alert>
+  );
+};
 
 const Root = () => {
-  return <Playground />;
+  return (
+    <main className="flex flex-col h-[calc(100vh-53px)] gap-3 bg-background p-4">
+      <WelcomeAlert />
+      <Playground
+        initProgram={""}
+        initInputs={{}}
+        initOutputs={{}}
+      />
+    </main>
+  );
 };
 
 export default Root;
