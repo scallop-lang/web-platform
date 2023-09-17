ffd9f891d0e38b02f260c4849705944f64c06fdf
Anthony <75352073+geant04@users.noreply.github.com>
2023-09-16 22:20:31 -0400

    Port a benchmark (#51)
    * stuff
    
    * updated prisma
    
    * updated prisma
    
    * update
    
    * adjust next

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
