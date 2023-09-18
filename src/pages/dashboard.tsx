5acc45682e560bebc446d86d44852d8f6ab3f924
Charles Wang <czw@seas.upenn.edu>
2023-09-18 19:02:17 -0400

    mutate loading states

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, LogIn, Plus } from "lucide-react";
+import { ArrowRight, Loader, LogIn, Plus } from "lucide-react";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter, type NextRouter } from "next/router";
+import React, { useState } from "react";
 
 import { Button } from "~/components/ui/button";
 import {
@@ -27,6 +28,8 @@ const ProjectCard = ({
   description: string | null;
   createdAt: Date;
 }) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <Card className="w-[350px] h-[200px]">
       <CardHeader>
@@ -40,9 +43,23 @@ const ProjectCard = ({
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Button onClick={() => router.push(`/project/${projectId}`)}>
-          <ArrowRight className="mr-2 w-4 h-4" />
-          Go to project
+        <Button
+          onClick={async () => {
+            setLoading(true);
+            await router.push(`/project/${projectId}`);
+          }}
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              <Loader className="mr-2 w-4 h-4" /> Loading...
+            </>
+          ) : (
+            <>
+              <ArrowRight className="mr-2 w-4 h-4" />
+              Go to project
+            </>
+          )}
         </Button>
       </CardContent>
     </Card>
@@ -54,21 +71,19 @@ const Dashboard = () => {
   const { toast } = useToast();
   const router = useRouter();
 
-  const { data, isLoading, isError, error } =
+  const { data: projectData, isLoading: projectIsLoading } =
     api.project.getProjectsByUser.useQuery();
 
-  const { mutate } = api.project.create.useMutation({
-    onSuccess: async (projectData) => {
-      await router.push(`/project/${projectData.id}`);
-    },
-  });
-
-  if (isError) {
-    toast({
-      title: "An error occurred",
-      description: `Something happened while loading your projects. Reason: ${error.message}`,
-    });
-  }
+  const { mutate, isLoading: createIsLoading } = api.project.create.useMutation(
+    {
+      onSuccess: async ({ id }) => await router.push(`/project/${id}`),
+      onError: (error) =>
+        toast({
+          title: "An error occurred",
+          description: `Something happened while creating a new project. Reason: ${error.message}`,
+        }),
+    }
+  );
 
   if (status === "loading") {
     return (
@@ -83,14 +98,14 @@ const Dashboard = () => {
   }
 
   if (status === "authenticated") {
-    const projectsList: React.ReactNode = isLoading ? (
+    const projectsList: React.ReactNode = projectIsLoading ? (
       <Skeleton className="w-[350px] h-[200px] rounded-lg flex items-center justify-center">
         <p className="text-sm text-muted-foreground">
           Loading your projects...
         </p>
       </Skeleton>
-    ) : data ? (
-      data.map((project, index) => (
+    ) : projectData ? (
+      projectData.map((project, index) => (
         <ProjectCard
           key={index}
           router={router}
@@ -111,23 +126,33 @@ const Dashboard = () => {
     const name = session.user?.name ? session.user.name : "Scallop user";
     const first = name.split(" ")[0];
 
+    const createProjectButton = createIsLoading ? (
+      <Card className="w-[350px] p-6 bg-muted transition h-[200px] flex flex-col items-center justify-center">
+        <Loader className="w-8 h-8" />
+        <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          Creating new project...
+        </h3>
+        <p className="text-sm">Hold on tight.</p>
+      </Card>
+    ) : (
+      <Card className="w-[350px] p-6 hover:bg-muted transition h-[200px] flex flex-col items-center justify-center">
+        <Plus className="w-8 h-8 text-muted-foreground" />
+        <h3 className="scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground ">
+          Create new project
+        </h3>
+        <p className="text-sm text-muted-foreground">
+          Start an empty project from scratch.
+        </p>
+      </Card>
+    );
+
     return (
       <main className="flex flex-col space-y-3 min-h-screen bg-background p-4">
         <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
           Welcome back, {first ? first : name}.
         </h3>
         <div className="flex gap-4 flex-wrap">
-          <button onClick={() => mutate()}>
-            <Card className="w-[350px] p-6 hover:bg-muted transition h-[200px] flex flex-col items-center justify-center">
-              <Plus className="w-8 h-8 text-muted-foreground" />
-              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground ">
-                Create new project
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Start an empty project from scratch.
-              </p>
-            </Card>
-          </button>
+          <button onClick={() => mutate()}>{createProjectButton}</button>
           {projectsList}
         </div>
       </main>
