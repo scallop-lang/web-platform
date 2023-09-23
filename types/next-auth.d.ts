6a364db8ad11e5f5c1c323377adff6f1818554f7
Charles Wang <czw@seas.upenn.edu>
2023-09-23 15:56:17 -0400

    add auth roles (#63)

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,5 +1,7 @@
+import type { Role } from "@prisma/client";
 import { type DefaultSession } from "next-auth";
 
+// see https://authjs.dev/getting-started/typescript#main-module
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
@@ -7,6 +9,7 @@ declare module "next-auth" {
   interface Session {
     user?: {
       id?: string;
+      role?: Role;
     } & DefaultSession["user"];
   }
 }
