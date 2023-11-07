0c03e6d38b97cd5ec179a4626cb8d5aae413d92f
Charles Wang <czw@seas.upenn.edu>
2023-11-07 14:20:10 -0500

    delete all of me and anthony's hard work

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -1,11 +1,12 @@
+import { Card } from "~/components/ui/card";
+
 import CodeEditor from "./code-editor";
-import TableEditor from "./table-editor";
 
 const Playground = () => {
   return (
     <div className="grid h-full auto-rows-fr grid-cols-2 gap-3">
       <CodeEditor />
-      <TableEditor />
+      <Card className="col-span-1 h-full grow p-4 text-sm">panel</Card>
     </div>
   );
 };
