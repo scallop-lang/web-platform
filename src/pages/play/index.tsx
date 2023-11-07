f9d4a59a4c82e9532d009f500636675726a494c2
Charles Wang <czw@seas.upenn.edu>
2023-11-07 14:24:25 -0500

    remove welcome popup for now

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,46 +1,8 @@
-import { PartyPopper } from "lucide-react";
-import Link from "next/link";
-import { useState } from "react";
-
 import Playground from "~/components/playground";
-import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
-
-const WelcomeAlert = () => {
-  const [closed, setClosed] = useState(false);
-
-  if (closed) {
-    return null;
-  }
-
-  return (
-    <Alert className="shrink-0">
-      <PartyPopper className="mr-2 h-5 w-5" />
-      <AlertTitle>Welcome to Scallop!</AlertTitle>
-      <AlertDescription>
-        If you close the playground all work will be lost. Make an account to
-        access the{" "}
-        <Link
-          href="/dashboard"
-          className="font-medium underline"
-        >
-          dashboard
-        </Link>{" "}
-        and to save your projects.{" "}
-        <button
-          className="font-medium underline"
-          onClick={() => setClosed(true)}
-        >
-          Close this alert.
-        </button>
-      </AlertDescription>
-    </Alert>
-  );
-};
 
 const Root = () => {
   return (
     <main className="flex h-[calc(100vh-53px)] flex-col gap-3 bg-background p-4">
-      <WelcomeAlert />
       <Playground />
     </main>
   );
