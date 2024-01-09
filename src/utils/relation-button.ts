34fd742ef109c1a8819e07d7b51617b8b0616a5c
GeneralPoxter <generalpoxter@gmail.com>
2024-01-08 20:03:00 -0500

    print out relation facts

--- src/utils/relation-button.ts
+++ src/utils/relation-button.ts
@@ -4,12 +4,12 @@ import type { DecorationSet, EditorView, ViewUpdate } from "@codemirror/view";
 import { Decoration, ViewPlugin, WidgetType } from "@codemirror/view";
 
 class RelationWidget extends WidgetType {
-  constructor(readonly relationName: string) {
+  constructor(readonly relationStart: number) {
     super();
   }
 
   eq(other: RelationWidget) {
-    return other.relationName == this.relationName;
+    return other.relationStart == this.relationStart;
   }
 
   toDOM() {
@@ -18,7 +18,7 @@ class RelationWidget extends WidgetType {
     wrap.className = "scl-relation-button";
     const btn = wrap.appendChild(document.createElement("input"));
     btn.type = "button";
-    btn.name = this.relationName;
+    btn.id = this.relationStart.toString();
     btn.setAttribute(
       "style",
       "vertical-align: middle; font-size: 8px; font-weight: bold; margin-left: 4px; border: 1px solid black; border-radius: 20px; cursor: pointer;",
@@ -39,18 +39,53 @@ function relationButtons(view: EditorView) {
       from,
       to,
       enter: (node) => {
-        if (node.name == "RelationIdentifier") {
-          const relationName = view.state.doc.sliceString(node.from, node.to);
+        const relationIdNode = node.node.getChild("RelationIdentifier");
+        const factSetNode = node.node.getChild("FactSet");
+        if (node.name == "FactSetDecl" && relationIdNode && factSetNode) {
           const deco = Decoration.widget({
-            widget: new RelationWidget(relationName),
+            widget: new RelationWidget(relationIdNode.from),
             side: 1,
           });
-          widgets.push(deco.range(node.to));
+          widgets.push(deco.range(relationIdNode.to));
         }
       },
     });
   }
   return Decoration.set(widgets);
+}
+
+function getTable(view: EditorView, relationStart: number) {
+  let relationName;
+  const relationFacts: string[][] = [];
+
+  for (const { from, to } of view.visibleRanges) {
+    syntaxTree(view.state).iterate({
+      from,
+      to,
+      enter: (node) => {
+        const relationIdNode = node.node.getChild("RelationIdentifier");
+        const factSetNode = node.node.getChild("FactSet");
+        if (node.name == "FactSetDecl" && relationIdNode && factSetNode) {
+          if (relationIdNode.from == relationStart) {
+            relationName = view.state.doc.sliceString(relationIdNode.from, relationIdNode.to);
+            factSetNode.getChildren("ListItem").forEach((fact) => {
+              const tuple: string[] = [];
+              const tupleNode = fact.getChild("ConstTuple");
+              if (tupleNode) {
+                tupleNode.getChildren("ListItem").forEach((constant) => {
+                  tuple.push(view.state.doc.sliceString(constant.from, constant.to));
+                });
+              }
+              relationFacts.push(tuple);
+            });
+          }
+        }
+      },
+    });
+  }
+
+  console.log(relationName);
+  console.log(relationFacts);
 }
 
 export const relationButtonPlugin = ViewPlugin.fromClass(
@@ -70,13 +105,13 @@ export const relationButtonPlugin = ViewPlugin.fromClass(
     decorations: (v) => v.decorations,
 
     eventHandlers: {
-      mousedown: (e) => {
+      mousedown: (e, view) => {
         const target = e.target as HTMLElement;
         if (
           target.nodeName == "INPUT" &&
           target.parentElement!.classList.contains("scl-relation-button")
         )
-          console.log(target.getAttribute("name"));
+          getTable(view, parseInt(target.id ?? ""));
       },
     },
   },
