cf989b9b4066195eacac47585d099484a6a447a2
Anthony <geant@seas.upenn.edu>
2024-01-12 15:55:16 -0500

    save current changes

--- src/components/google-picker.tsx
+++ src/components/google-picker.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import useDrivePicker from "react-google-drive-picker";
 import type { CallbackDoc } from "react-google-drive-picker/dist/typeDefs";
 import { useToast } from "~/components/ui/use-toast";
@@ -9,20 +10,29 @@ const GooglePicker = ({
     changeEditorFunction: (value: string) => void
 }) => {
     const [openPicker] = useDrivePicker();
+    const [key, setKey] = useState<string | undefined>(undefined);
     const {toast} = useToast();
 
-    const loadFile = async (file : CallbackDoc) => {
+    const loadFile = async (file : CallbackDoc, token: string | undefined) => {
         try {
             console.log(file);
-            const response = await gapi.client.drive.files.get({
+            /*const response = await gapi.client.drive.files.get({
                 fileId: file.id,
                 alt:'media'
+            });*/
+            
+            await fetch(`https://www.googleapis.com/drive/v3/files/${file.id}?alt=media`, {
+                headers: new Headers({ 'Authorization': 'Bearer ' + token }),
+            }).then((res) => {
+                return res.text();
+            }).then((val) => {
+                changeEditorFunction(val);
             });
-            changeEditorFunction(response.body);
             toast({
                 description: 'Successfully loaded file!'
             })
         } catch (error) {
+            console.log(error);
             toast({
                 description: 'Error: ' + !error
             })
@@ -30,18 +40,16 @@ const GooglePicker = ({
     }
 
     const handleOpenPicker = () => {
-        function start() {
-            gapi.client.init({
-                'apiKey' : 'AIzaSyBDRsgSPlCLFUe6RZ6zNT12LaeA4ip7aa4',
-                'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
-            }).then(() => {
-                const tokenInfo = gapi.auth.getToken();
+        const client: google.accounts.oauth2.TokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: '494588134232-gvaumsid2ucu5ckfrh45oer4ku2ch1bf.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/drive.file',
+            prompt: key ? '' : 'select_account',
+            callback: (response) => {
+                setKey(response ? response.access_token : undefined);
                 openPicker({
                     clientId: '494588134232-gvaumsid2ucu5ckfrh45oer4ku2ch1bf.apps.googleusercontent.com',
                     developerKey: 'AIzaSyBDRsgSPlCLFUe6RZ6zNT12LaeA4ip7aa4',
-                    viewId: 'DOCS',
-                    viewMimeTypes: 'application/octet-stream',
-                    token: tokenInfo ? tokenInfo.access_token : undefined,
+                    token: response ? response.access_token : undefined,
                     callbackFunction(data) {
                           const elements = Array.from(
                             document.getElementsByClassName(
@@ -54,7 +62,7 @@ const GooglePicker = ({
                           if (data.action === 'picked') {
                             data.docs.map(async (item) => {
                                 if (item.name.endsWith('.scl')) {
-                                    await loadFile(item);
+                                    await loadFile(item, (response ? response.access_token : undefined));
                                 } else {
                                     toast({
                                         description: `File is not a Scallop file! You must pick a .scl file.`,
@@ -62,15 +70,11 @@ const GooglePicker = ({
                                 }
                             });
                         }
-                    },
+                    }
                 });
-            }).then((response) => {
-                console.log(response);
-            }, function() {
-                console.log('Error');
-            });
-        };
-        gapi.load("client:auth2", start);
+            }
+        });
+        client.requestAccessToken();
     };
 
     return (
--- src/components/save-to-drive.tsx
+++ src/components/save-to-drive.tsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "~/components/ui/button";
 import useDrivePicker from "react-google-drive-picker";
 import type { CallbackDoc } from "react-google-drive-picker/dist/typeDefs";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
+import { Label } from "./ui/label";
+import { Popover, PopoverTrigger, PopoverContent } from "@radix-ui/react-popover";
+import { FileDown } from "lucide-react";
+import { Input } from "./ui/input";
+import { useToast } from "./ui/use-toast";
 
 const SaveToDrive = ({
     program,
 } : {
     program: string
 }) => {
-
     const [openPicker] =  useDrivePicker();
+    const [key, setKey] = useState<string | undefined>(undefined);
+    const [filename, setFilename] = useState("raw");
+    const {toast} = useToast();
 
     // ONLY SAVES TO A FOLDER
     // Will work on saving to a file later
+    // you cannot switch emails tho hmmm
     const sendToDrive = async ({
         folder,
         content,
         filename,
+        token,
     } : {
         folder: CallbackDoc,
         content: string,
         filename: string
+        token?: string
     }) => {
         try {
-            const accessToken = gapi.auth.getToken().access_token;
+            const accessToken = token;
             const folderId = folder.id;
 
             const file = new Blob([content], {type: 'text/plain'});
@@ -46,11 +57,16 @@ const SaveToDrive = ({
                 body: form,
             }).then((res) => {
                 return res.json();
-            }).then(function(val) {
-                console.log(val);
+            }).then(function() {
+                toast({
+                    description: 'Successfully saved file!'
+                });
             });
-        } catch {
-            console.log('error');
+        } catch (error) {
+            console.log(error);
+            toast({
+                description: 'Error - Could not save file.'
+            });
         }
     }
 
@@ -58,7 +74,10 @@ const SaveToDrive = ({
         const client: google.accounts.oauth2.TokenClient = google.accounts.oauth2.initTokenClient({
             client_id: '494588134232-gvaumsid2ucu5ckfrh45oer4ku2ch1bf.apps.googleusercontent.com',
             scope: 'https://www.googleapis.com/auth/drive.file',
+            prompt: key ? '' : 'select_account',
             callback: (response) => {
+                setKey(response.expires_in);
+                console.log(response);
                 openPicker({
                     clientId: '494588134232-gvaumsid2ucu5ckfrh45oer4ku2ch1bf.apps.googleusercontent.com',
                     developerKey: 'AIzaSyBDRsgSPlCLFUe6RZ6zNT12LaeA4ip7aa4',
@@ -69,7 +88,10 @@ const SaveToDrive = ({
                     callbackFunction(data) {
                         if(data.action === 'picked') {
                             data.docs.map(async (item) => {
-                                await sendToDrive({folder: item, content: content, filename: filename});
+                                await sendToDrive({folder: item, 
+                                                   content: content, 
+                                                   filename: filename,
+                                                   token: response ? response.access_token : undefined});
                             });
                         }
                     }
@@ -80,12 +102,41 @@ const SaveToDrive = ({
     };
 
     return (
-        <Button
-            onClick = {() => {download(program, "test")}}
-            className = "mx-2"
-        >
-            Save To Drive
-        </Button>
+        // grabbed this code from the raw file component
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="mx-2">
+                Save to Drive Folder
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-4">
+                <DialogHeader>
+                <DialogTitle>Save to Drive</DialogTitle>
+                <DialogDescription>
+                    Save a raw Scallop (.scl) file to your drive. After clicking the button 
+                    below, you will be prompted to select a folder to save the file to.
+                </DialogDescription>
+                </DialogHeader>
+
+                <div className="mb-2 grid gap-2">
+                        <Label htmlFor="filename">Filename</Label>
+                        <Input
+                        type="text"
+                        placeholder="Filename (required)"
+                        id="filename"
+                        value={filename}
+                        onChange={(e) => setFilename(e.target.value)}
+                        />
+                </div>
+                <Button
+                    disabled={filename.length === 0}
+                    onClick={() => download(program, filename)}
+                    className="transition"
+                >
+                    <FileDown className="mr-2 h-4 w-4" /> Download file
+                </Button>
+            </DialogContent>
+        </Dialog>
     );
 };
 
