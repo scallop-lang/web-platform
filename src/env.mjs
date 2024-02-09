a0f7333141a9565af96e1dcf370b807e8d58abab
Charles Wang <czw@seas.upenn.edu>
2024-02-09 13:55:39 -0500

    fix t3-env issues

--- src/env.mjs
+++ src/env.mjs
@@ -2,10 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     FLASK_SERVER: z.string(),
@@ -15,17 +11,6 @@ export const env = createEnv({
     GITHUB_SECRET: z.string(),
   },
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  client: {},
-
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     FLASK_SERVER: process.env.FLASK_SERVER,
@@ -35,9 +20,5 @@ export const env = createEnv({
     GITHUB_SECRET: process.env.GITHUB_SECRET,
   },
 
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
-   * This is especially useful for Docker builds.
-   */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  skipValidation: Boolean(process.env.SKIP_ENV_VALIDATION),
 });
